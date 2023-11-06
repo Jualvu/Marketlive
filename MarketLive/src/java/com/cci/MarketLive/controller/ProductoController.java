97ee66461e8278ee1dc5a83024fd6ac64cfc3c31
julio <julio@PC-JULIO>
2023-11-05 22:31:07 -0600

    compras

--- MarketLive/src/java/com/cci/MarketLive/controller/ProductoController.java
+++ MarketLive/src/java/com/cci/MarketLive/controller/ProductoController.java
@@ -5,10 +5,35 @@
  */
 package com.cci.MarketLive.controller;
 
+import com.cci.MarketLive.service.ProductoTO;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author julio
  */
-public class ProductoController {
+
+@ManagedBean(name = "productoController")
+@SessionScoped
+
+
+public class ProductoController implements Serializable {
     
+    private List<ProductoTO> products;
+    private ProductoTO selectedProducto;
+
+     public ProductoController() {
+    }
+    
+
+    
+   
+
+
 }
